d7c12f2eef2ceba7ed600cb282bfe1249a563e7c
Jayce <jaycedenge1215@163.com>
2017-06-07 10:53:26 +0800

    能正常运行，可以播放语言，但有间隔BUG
    因为for循环一次性设定好了固定的播放间隔，无法等待语音播放完后再播放下一个语音

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 window.onload=function(){
 	var platform = "google";
-	var url = "https://translate.google.cn/translate_tts?ie=UTF-8&tl=en&total=1&idx=0&textlen=6&tk=287229.145022&client=t&prev=input";
+	var url = "https://translate.google.cn/translate_tts?ie=UTF-8&tl=zh-CN&total=1&idx=0&client=t&prev=input";
 	var config = {
 		random : getSelected($("#random")).value,
-		interval : getSelected($("#interval")).value,
-		repeatNum : getSelected($("#repeatNum")).value,
+		interval : parseInt(getSelected($("#interval")).value),
+		repeatNum : parseInt(getSelected($("#repeatNum")).value),
 		speed : (platform=='google'?'0.':'')+getSelected($("#speed")).value,
 	}
 
 	var beginBtn = $("#play");
 	beginBtn.onclick = function(){
 		var list = $("#list").children;
-		var tts = $("#tts");
+		var audio = $("#tts");
+		audio.loop = false;
+		var flag = true;
 		// 随机这里有待更改
 		var arr = [];
 		for(var i=0;i<list.length;i++){
@@ -19,17 +21,42 @@ window.onload=function(){
 		}
 		if(config.random){
 			arr.sort(function(){return (0.5-Math.random());});
-		}console.log(arr)
+		}
+		console.log(arr);
+		var timeout = config.interval;
 		// 随机这里有待更改
 		for(var i=0;i<arr.length;i++){
-			for(var r=0;r<config.repeatNum;r++){
-				(function(num){
-					setTimeout(function(){
-					var text = arr[num].textContent;
-					tts.src = url+"&q="+text+"&ttsspeed="+config.speed;
-					tts.play();
-					},config.interval)
-				}(i))
+			(function(num){
+				for(var r=0;r<config.repeatNum;r++){
+					time(num,timeout);
+					timeout+= config.interval;
+				}
+			}(i))
+		}
+		function time(num,out){
+			if(flag){
+				setTimeout(function(){
+					flag=false;
+					var c = 1;
+					var text = arr[num];
+					ajax({
+						method : "GET",
+						url : "http://tt.890m.com/translate_tkk.php",
+						success : function(tkk){
+							ik = tkk;
+							tk = jk(text);
+							audio.src = url+"&tk="+tk+"&q="+encodeURIComponent(text)+"&ttsspeed="+config.speed+"&=textlen="+text.length;
+							audio.play();
+							audio.addEventListener('ended',function(e){
+								if(c==1){
+									console.log('播放完');
+									flag=true;
+									c=0;
+								}
+							});
+						}
+					})
+				},out);
 			}
 		}
 	}
